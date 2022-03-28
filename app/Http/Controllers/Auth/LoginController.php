7613f803d489e18e21e314ca65fae52379c1703a
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2022-03-28 17:22:31 -0500

    Login insecure http test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
         ]);
         if (Auth::attempt($credentials, 'remember')) {
             //$request->session()->regenerate();
-            return back()->with('success', 'Login Successful');
+            return redirect()->back()->with('message', 'Login Successful');
         } else {
             $message = 'Email o password incorrecto(s)';
             return back()->withErrors([
